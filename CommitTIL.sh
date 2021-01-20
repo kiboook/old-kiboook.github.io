3b64326d797584d970babbae07bc526aaafffa08
Nam-Ki-Bok <nkb7714@naver.com>
2021-01-21 00:36:52 +0900

    [2020.01.21] TIL 커밋 자동화

--- CommitTIL.sh
+++ CommitTIL.sh
@@ -1,23 +1,21 @@
-echo "Now : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
-
 YEAR=$(date +%Y)
 MONTH=$(date +%m)
 DAY=$(date +%d)
 
-echo "> Git add TIL ..."
+echo "> Git add TIL ...\n"
 
 git add _posts/TDL/$YEAR-$MONTH-$DAY-$(date +%y)$MONTH$(date +%d)TDL.md
 
 sleep 1
 
-echo "> Git Commit ..."
+echo "> Git Commit ...\n"
 
 git commit -m "[$YEAR.$MONTH.$DAY] Done :)"
 
 sleep 1
 
-echo "> Git Push ..."
+echo "\n> Git Push ...\n"
 
 git push -u origin master
 
-echo "> SUCCESS PUSH [$YEAR.$MONTH.$DAY] TDL"
+echo "\n> SUCCESS PUSH [$YEAR.$MONTH.$DAY] TIL"
